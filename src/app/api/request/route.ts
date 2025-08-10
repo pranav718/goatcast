92e23700610720e79a200eddc6cafb34f8b43f7f
pranav718 <raypranav718@gmail.com>
2025-08-10 12:58:04 +0530

    added basic rate limiting

--- src/app/api/request/route.ts
+++ src/app/api/request/route.ts
@@ -37,7 +37,35 @@ function extractChannelHandle(url: string): string | null{
 
 }
 
+const rateLimitStore = new Map<string,number>();
+
+function getRateLimit(ip: string): boolean{
+    const now = Date.now();
+    const lastRequest = rateLimitStore.get(ip) || 0;
+    const cooldown = 15000;
+
+    if(now - lastRequest < cooldown){
+        return false;
+    }
+
+    rateLimitStore.set(ip,now);
+    return true;
+}
 
 export async function POST(request: NextRequest){
-    return NextResponse.json({message: ""});
+    
+    try {
+        const forwarded = request.headers.get('x-forwarded-for');
+        const ip = forwarded ? forwarded.split(',')[0].trim() : 'unknown';
+        
+        if(!getRateLimit(ip)){
+            return NextResponse.json(
+                {error: "please wait 15 seconds before submitting again."},
+                {status: 429}
+            )
+        }
+        
+    } catch (error) {
+        console.error('Error extracting IP:', error);
+    }
 }
